fc4fad8f448509dcfab52018630ba1203191f8c7
dave8879 <davidmanych@seznam.cz>
2023-03-04 21:21:08 +0100

    jquary tlačítko

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,17 @@
-var button = document.querySelector("#button");
-var modal = document.querySelector("#modal");
-var close = document.querySelector("#modal");
 
-button.addEventListener("click", function(){
-    modal.classList.add("modal_active")
+
+$(document).ready(function(){
+var button= $("#button");
+var modal= $("#modal");
+var close= $("#close");
+
+
+button.on("click", function(){
+    modal.addClass("modal_active")
 });
 
-close.addEventListener("click", function(){
-    modal.classList.remove("modal_active")
+close.on("click", function(){
+    modal.removeClass("modal_active")
+});
+
 });
